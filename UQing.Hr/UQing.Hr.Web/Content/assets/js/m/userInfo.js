34ed715eb6fda1f1c44bd32e64f89f0fa7358666
tonyyzz <1277745281@qq.com>
2017-04-14 17:47:55 +0800

    1、新闻资讯数据与搜索功能
    2、会员页面布局，及会员信息的修改

---
+++ UQing.Hr/UQing.Hr.Web/Content/assets/js/m/userInfo.js
@@ -0,0 +1,179 @@
+﻿
+$(function () {
+	(function (window) {
+		init();
+		function init() {
+			getPerInfo();
+
+		}
+
+		function getPerInfo() {
+			$.ajax({
+				url: "/m/getperinfo",
+				type: "post",
+				datatype: "json",
+				timeout: 5000,
+				success: function (resp) {
+					if (resp.result == 1) {
+						if (resp.data.idt == 1) {
+							setPerInfoHtml(resp.data.person);
+						} else {
+
+						}
+					} else {
+
+					}
+				},
+				complete: function (XMLHttpRequest, status) {
+					if (status == 'timeout') {	//超时
+
+					} else if (XMLHttpRequest.status != "200") {	//返回失败状态
+
+					}
+				}
+			})
+		}
+
+		function setPerInfoHtml(person) {
+			$("#perHeadImg").attr("src", ((!!person.Photo) ? comHelper.resourceHost + person.Photo : "/Content/assets/img/m/no_photo_male.png"));
+			$("#realname").val((person.RealName ? person.RealName : ""));
+			showSexHtml(person);
+			$("#residence").val((!!person.City) ? person.City : "");
+			$("#education").html((!!person.Education) ? person.Education : "本科");
+			$("#workLife").html((!!person.WorkLife) ? person.WorkLife : "应届毕业生");
+			$("#phone").val((!!person.Phne) ? person.Phne : "");
+			$("#email").val((!!person.Email) ? person.Email : "");
+		}
+		function showSexHtml(person) {
+			var isWoman = (person.Sex === false);
+			var $gender = $("#gender");
+			$gender.children(".J_radioitme").removeClass("checked");
+			if (!isWoman) {
+				$gender.children(".J_radioitme").eq(0).addClass("checked");
+			} else {
+				$gender.children(".J_radioitme").eq(1).addClass("checked");
+			}
+		}
+
+		eventBind();
+		function eventBind() {
+			genderSwitchEvent();
+			saveEvent();
+		}
+		function genderSwitchEvent() {
+			var $gender = $("#gender");
+			$gender.children(".J_radioitme").unbind("click");
+			$gender.children(".J_radioitme").bind("click", function () {
+				var $that = $(this);
+				$gender.children(".J_radioitme").removeClass("checked");
+				$that.addClass("checked");
+			});
+		}
+		function saveEvent() {
+			$("#btnSavePerInfo").unbind("click");
+			$("#btnSavePerInfo").bind("click", function () {
+				var realname = $("#realname").val();
+				if (!realname || !$.trim(realname)) {
+					comTipsHelper.disappearTips('remind', '请输入姓名');
+					return;
+				}
+				var $gender = $("#gender");
+				var $genderChecked = $gender.children(".checked");
+				var gender = 1;
+				if (!$genderChecked || $genderChecked.length <= 0) {
+					comTipsHelper.disappearTips('remind', '请选择性别');
+					return;
+				}
+				gender = $genderChecked.data("gender");
+				if (!gender || (gender != 1 && gender != 2)) {
+					gender = 1; //1：男(默认)（2：女）
+				}
+				var residence = $("#residence").val();
+				if (!residence || !$.trim(residence)) {
+					residence = '';
+				}
+				var education = $("#education").text();
+				if (!education || !$.trim(education)) {
+					education = '本科';
+				}
+				var workLife = $("#workLife").text();
+				if (!workLife || !$.trim(workLife)) {
+					workLife = '应届毕业生';
+				}
+				var phone = $("#phone").val();
+				if (!phone || !$.trim(phone)) {
+					comTipsHelper.disappearTips('remind', '请填写手机号');
+					return;
+				}
+				if (!memberHelper.isMatchPhone(phone)) {
+					comTipsHelper.disappearTips('remind', '请填写正确的手机号');
+					return;
+				}
+				var email = $("#email").val();
+				if (!email || !$.trim(email)) {
+					comTipsHelper.disappearTips('remind', '请填写邮箱');
+					return;
+				}
+				if (!memberHelper.isMatchEmail(email)) {
+					comTipsHelper.disappearTips('remind', '请填写正确的邮箱');
+					return;
+				}
+				$.ajax({
+					url: "/m/saveperinfo",
+					type: "post",
+					datatype: "json",
+					data: {
+						realname: realname,
+						gender: gender,
+						residence: residence,
+						education: education,
+						workLife: workLife,
+						phone: phone,
+						email: email
+					},
+					timeout: 5000,
+					success: function (resp) {
+						if (resp.result == 1) {
+							//保存成功
+							comTipsHelper.disappearTips('success', '保存成功', function () {
+								yHelper.response.redirect("/m");
+							});
+						} else if (resp.result == 2) {
+							if (resp.result.flag == 1) { //求职者不存在
+
+							} else if (resp.result.flag == 2) { //身份错误
+
+							} else { //非法操作
+
+							}
+						} else if (resp.result == 0) {
+							if (resp.result.flag == 1) { //姓名不能为空
+								comTipsHelper.disappearTips('remind', '请输入姓名');
+							} else if (resp.result.flag == 2) { //学历不能为空
+								comTipsHelper.disappearTips('remind', '请输入学历');
+							} else if (resp.result.flag == 3) { //工作经验不能为空
+								comTipsHelper.disappearTips('remind', '请输入工作经验');
+							} else if (resp.result.flag == 4) { //手机号码非法
+								comTipsHelper.disappearTips('remind', '请填写正确的手机号');
+							} else if (resp.result.flag == 5) { //邮箱非法
+								comTipsHelper.disappearTips('remind', '请填写正确的邮箱');
+							} else { //非法操作
+
+							}
+						} else { //非法操作
+
+						}
+
+					},
+					complete: function (XMLHttpRequest, status) {
+						if (status == 'timeout') {	//超时
+
+						} else if (XMLHttpRequest.status != "200") {	//返回失败状态
+
+						}
+					}
+				})
+			});
+		}
+	})(window)
+})
--- UQing.Hr/UQing.Hr.Web/Controllers/MController.cs
+++ UQing.Hr/UQing.Hr.Web/Controllers/MController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using UQing.Hr.IServices;
+using UQing.Hr.Model.User;
 using UQing.Hr.WebHelper;
 
 namespace UQing.Hr.Web.Controllers
@@ -10,19 +13,148 @@ namespace UQing.Hr.Web.Controllers
 	/// <summary>
 	/// 会员中心
 	/// </summary>
-    public class MController : BaseController
-    {
-        /// <summary>
-        /// 会员中心首页
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class MController : BaseController
+	{
+		public MController(IPersonServices _PersonServices)
+		{
+			base._PersonServices = _PersonServices;
+		}
+		/// <summary>
+		/// 会员中心首页
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+
 		public ActionResult UserInfo()
 		{
 			return View();
 		}
-    }
+
+		/// <summary>
+		/// 获取求职者信息
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public ActionResult GetPerInfo()
+		{
+			var userInfo = UserManage.GetCurrentUserInfo();
+			if (userInfo == null)
+			{
+				//未登录
+				return GetJson(0, new { flag = 1 });
+			}
+			if (userInfo.IdentityType == IdentityType.Person)
+			{
+				var person = _PersonServices.QueryWhere(where => where.PerID == userInfo.UserId).FirstOrDefault();
+				if (person == null)
+				{
+					//求职者不存在
+					return GetJson(2, new { flag = 1 });
+				}
+				return GetJson(1, new { idt = (int)userInfo.IdentityType, person = person });
+			}
+			else
+			{
+				//身份错误
+				return GetJson(0, new { flag = 2 });
+			}
+		}
+		/// <summary>
+		/// 求职者信息修改保存
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public ActionResult SavePerInfo(FormCollection forms)
+		{
+			var userInfo = UserManage.GetCurrentUserInfo();
+			if (userInfo == null)
+			{
+				//未登录
+				return GetJson(-1, new { flag = 1 });
+			}
+			if (userInfo.IdentityType == IdentityType.Person)
+			{
+				var person = _PersonServices.QueryWhere(where => where.PerID == userInfo.UserId).FirstOrDefault();
+				if (person == null)
+				{
+					//求职者不存在
+					return GetJson(2, new { flag = 1 });
+				}
+				//保存
+				string realname = forms["realname"] ?? "";
+				string gender = forms["gender"] ?? "";
+				string residence = forms["residence"] ?? "";
+				string education = forms["education"] ?? "";
+				string workLife = forms["workLife"] ?? "";
+				string phone = forms["phone"] ?? "";
+				string email = forms["email"] ?? "";
+				if (string.IsNullOrWhiteSpace(realname))
+				{
+					//姓名不能为空
+					return GetJson(0, new { flag = 1 });
+				}
+				if (string.IsNullOrWhiteSpace(gender))
+				{
+					gender = "1";
+				}
+				bool sex = true; //默认男
+				if (gender == "2")
+				{
+					sex = false;
+				}
+				if (string.IsNullOrWhiteSpace(residence))
+				{
+					residence = "";
+				}
+				if (string.IsNullOrWhiteSpace(education))
+				{
+					//学历不能为空
+					return GetJson(0, new { flag = 2 });
+				}
+				if (string.IsNullOrWhiteSpace(workLife))
+				{
+					//工作经验不能为空
+					return GetJson(0, new { flag = 3 });
+				}
+				if (string.IsNullOrWhiteSpace(phone) || !Regex.IsMatch(phone, @"^1\d{10}$"))
+				{
+					//手机号码非法
+					return GetJson(0, new { flag = 4 });
+				}
+				if (string.IsNullOrWhiteSpace(email) || !Regex.IsMatch(email, @"^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$"))
+				{
+					//邮箱非法
+					return GetJson(0, new { flag = 5 });
+				}
+				person.RealName = realname;
+				person.Sex = sex;
+				person.City = residence;
+				person.Education = education;
+				person.WorkLife = workLife;
+				person.Phne = phone;
+				person.Email = email;
+				var properties = new string[] { "RealName", "Sex", "City", "Education", "WorkLife", "Phne", "Email" };
+				_PersonServices.Edit(person, properties);
+				if (_PersonServices.SaveChanges() > 0)
+				{
+					//保存成功
+					return GetJson(1);
+				}
+				else
+				{
+					//保存失败
+					return GetJson(3);
+				}
+			}
+			else
+			{
+				//身份错误
+				return GetJson(2, new { flag = 2 });
+			}
+		}
+	}
 }
